d352155f9f41bfe91bd946737323e5a269cc1d22
Sheng-Yi Hsu <Sheng-Yi.Hsu@asml.com>
2021-10-19 19:39:29 +0200

    init commit

---
+++ notebooks/config.py
@@ -0,0 +1,5 @@
+exp_configs = {
+    1: {"param1": 1, "param2": 3, "description":"exp1"},
+    2: {"param1": 1, "param2": 3, "description":"exp2"},
+    3: {"param1": 1, "param2": 3, "description":"exp3"},
+}
