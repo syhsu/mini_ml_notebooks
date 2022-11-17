51d3fbc9036fece20566a19d20a8063cf127efe9
shehsu <88491163+shehsu@users.noreply.github.com>
2022-11-17 15:22:36 +0100

    Update config.py

--- notebooks/config.py
+++ notebooks/config.py
@@ -3,3 +3,9 @@ exp_configs = {
     2: {"param1": 1, "param2": 3, "description":"exp2"},
     3: {"param1": 1, "param2": 3, "description":"exp3"},
 }
+
+PROJECT_FOLDER = '../'
+DATA_FOLDER = os.path.join(PROJECT_FOLDER, "data")
+MODEL_FOLDER = os.path.join(PROJECT_FOLDER, "models")
+ARTIFACT_FOLDER = os.path.join(PROJECT_FOLDER, "artifacts")
+model_majorVer = 1 # majorVer denotes which experiment is
