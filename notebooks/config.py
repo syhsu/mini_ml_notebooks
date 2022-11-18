f99076d348b533575ff646abd2a6d0348f97db34
Sheng-yi Hsu <shengyihsu@gmail.com>
2022-11-18 18:40:15 +0100

    Update config.py

--- notebooks/config.py
+++ notebooks/config.py
@@ -1,3 +1,8 @@
+PROJECT_FOLDER = '../'
+DATA_FOLDER = os.path.join(PROJECT_FOLDER, "data")
+MODEL_FOLDER = os.path.join(PROJECT_FOLDER, "models")
+ARTIFACT_FOLDER = os.path.join(PROJECT_FOLDER, "artifacts")
+
 exp_configs = {
     1: {"param1": 1, "param2": 3, "description":"exp1"},
     2: {"param1": 1, "param2": 3, "description":"exp2"},
