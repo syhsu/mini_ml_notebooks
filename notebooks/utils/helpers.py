abfa314d9e6f81d2f5070bbf3c7fb793caf8f333
shengyihsu <shengyihsu@gmail.com>
2023-10-28 22:43:41 +0200

    remove redundant code

--- notebooks/utils/helpers.py
+++ notebooks/utils/helpers.py
@@ -1,9 +1,1 @@
 """helper functions"""
-
-
-def combine_config_results(config: dict, results: dict) -> dict:
-    """Add configs to results"""
-    for key, result in results.items():
-        result.update(config[key])
-
-    return results
