8b28a530ee47e655cbf17df44eb48c3a68cbb65e
shengyihsu <shengyihsu@gmail.com>
2023-10-30 10:39:30 +0100

    update exp

--- notebooks/params.py
+++ notebooks/params.py
@@ -2,5 +2,6 @@ exp_configs = {
     1: {"param1": 1, "param2": 3, "description":"exp1"},
     2: {"param1": 1, "param2": 3, "description":"exp2"},
     3: {"param1": 1, "param2": 3, "description":"exp3"},
-    4: {"param1": 1, "param2": 4, "description":"exp3"},
+    4: {"param1": 1, "param2": 4, "description":"exp4"},
+    5: {"param1": 1, "param2": 5, "description":"exp5"},
 }
